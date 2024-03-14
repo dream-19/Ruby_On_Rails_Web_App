570ae293e87ef9883709e806547b13158018cc2f
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2024-03-14 15:54:00 +0100

    trash

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    unlocks: 'users/unlocks'
+    registrations: 'normals/registrations'
   }
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/normals/registrations_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Normals::RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
