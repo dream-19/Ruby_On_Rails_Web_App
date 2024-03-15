a70a8db958fed7a7738229e48719c42a50c18d2d
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2024-03-15 12:23:16 +0100

    add modals fix mistakes

--- app/models/company_organizer.rb
+++ app/models/company_organizer.rb
@@ -4,6 +4,7 @@ class CompanyOrganizer < User
     validates :name, :email, :phone, presence: true
     validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
     
-    # An organizer can create many events
-    has_many :events, dependent: :destroy
+    # An organizer can create many events (and the events has a foreign key 'user_id')
+    # when and organizer deletes its account all the events created by him will be deleted
+    has_many :events, foreign_key: "user_id", dependent: :destroy
   end
--- app/models/user_organizer.rb
+++ app/models/user_organizer.rb
@@ -5,6 +5,7 @@ class UserOrganizer < User
     validates :date_of_birth, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format YYYY-MM-DD" }
     validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
     
-    # An organizer can create many events
-    has_many :events, dependent: :destroy
+    # An organizer can create many events (and the events has a foreign key 'user_id')
+    # when and organizer deletes its account all the events created by him will be deleted
+    has_many :events, foreign_key: "user_id", dependent: :destroy
   end
