a4d202a704626dc532b79b031e84ee07e4a53a7e
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2024-03-15 12:52:02 +0100

    add validation for length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  # validates the length of all the possible fields:
+  validates :name, :surname,:email, :phone,:address, :cap, :province, :city, :state, length: { maximum: 255, too_long: "must be at most %{count} characters" }
+
+
   validates :type, presence: true
   # The type of the user must be one of the following: UserNormal, UserOrganizer, CompanyOrganizer
   validates :type, inclusion: { in: UserRoles::ALL_ROLES, message: "must be one of the following: #{UserRoles::ALL_ROLES.join(', ')}" }
