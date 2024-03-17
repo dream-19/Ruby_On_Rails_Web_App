23273df8be40b3b179c623e80923f5ae08d67934
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2024-03-17 15:57:08 +0100

    countries suggestions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -27,8 +27,23 @@ document.addEventListener("turbo:load", () => {
       });
     });
 
+    //LOCATIONS 
+    const username = 'helloworld';
+    var countryNames = [];
 
+    //Fetch the list of all the countries when the form are loaded
+    const formsWithLocations = document.querySelectorAll('.form-with-locations');
   
+    if(formsWithLocations.length > 0) {
+      fetch(`http://api.geonames.org/countryInfoJSON?username=${username}`)
+        .then(response => response.json())
+        .then(data => {
+          // List of all the contries names
+          countryNames = data.geonames.map(country => country.countryName);
+        })
+        .catch(error => console.error('Error fetching countries:', error));
+    }
+
 
   /*fetch(`http://api.geonames.org/searchJSON?name_startsWith=${startsWith}&country=${countryCode}&username=${username}`)
     .then(response => response.json())
@@ -40,37 +55,36 @@ document.addEventListener("turbo:load", () => {
     })
     .catch(error => console.error('Error fetching cities:', error)); */
 
-    const username = 'helloworld';
     // Fetch and display country suggestions
-    const countryInput = document.getElementById('user_country');
-    const countrySuggestions = document.getElementById('countrySuggestions');
+    var countryInput = document.getElementById('user_country');
+    var countrySuggestions = document.getElementById('countrySuggestions');
   
-    countryInput.addEventListener('input', function() {
-      const query = countryInput.value;
-      if (query.length >= 1) { // Start suggesting after 2 characters
-        fetch(`http://api.geonames.org/searchJSON?q=${query}&featureClass=P&username=${username}`)
-          .then(response => response.json())
-          .then(data => {
-            countrySuggestions.innerHTML = ''; // Clear previous suggestions
-            console.log(data.geonames);
-            data.geonames.forEach(country => {
-              //console.log(country.countryName); //print country name to console
-              const div = document.createElement('div');
-              div.textContent = country.countryName; // Use countryName for GeoNames
-              div.style.cursor = 'pointer';
-              div.addEventListener('click', () => {
-                countryInput.value = country.countryName; // Fill input on click
-                countrySuggestions.innerHTML = ''; // Clear suggestions
-              });
-              countrySuggestions.appendChild(div);
+    if (countryInput && countrySuggestions) {
+      countryInput.addEventListener('input', function() {
+        const query = countryInput.value;
+        if (query.length >= 1 && countryNames.length > 0) { // Start suggesting after 1 characters
+          countrySuggestions.innerHTML = '';
+          // Filter the list of countries based on the input, keep only the countries that start with the input
+          const filteredCountries = countryNames.filter(country => country.toLowerCase().startsWith(query.toLowerCase()));
+         
+          // Display the filtered countries
+          filteredCountries.forEach(country => {
+            const div = document.createElement('div');
+            div.textContent = country;
+            div.style.cursor = 'pointer';
+            div.classList.add('list-group-item');
+            div.addEventListener('click', () => {
+              countryInput.value = country;
+              countrySuggestions.innerHTML = ''; // Clear suggestions
             });
-          })
-          .catch(error => console.error('Error:', error));
-      } else {
-        console.log("clear suggestions")
-        countrySuggestions.innerHTML = ''; // Clear suggestions if input is less than 2 characters
-      }
-    });
+            countrySuggestions.appendChild(div);
+          });
+          
+        } else {
+          countrySuggestions.innerHTML = ''; // Clear suggestions if input is less than 1 characters
+        }
+      });
+    }
 
   });
   
